08bc7e23f0ffaa058d12645d4c24ba64908caf21
absam-bc <abdisalanahmed95@gmail.com>
2026-06-25 08:29:03 +0300

    done

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,33 @@
 // Write your code here
+const products=['laptop','phone','Headphone','Monitor'];
 
+function logFirstProduct(productName){
+   console.log(products[0]);
+}
+
+console.log(logFirstProduct());
+
+function addNewProduct(item){
+  products.push(item);
+};
+
+addNewProduct('watch');
+
+console.log(products);
+
+function updateProduct(index,newName){
+   products[index]=newName;
+}
+updateProduct(0,'charger');
+
+console.log(products);
+
+function removeLastProduct(){
+  products.pop();
+}
+
+removeLastProduct();
+console.log(products);
 
 
 // Export the necessary parts for testing
